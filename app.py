2391a96dd7256cea6aeb7279ad2883bde0d30c61
im-808 <25hw@sookmyung.ac.kr>
2026-05-12 00:16:13 +0900

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ ORDER BY avg_usage DESC
 """
 df2 = run_query(sql2)
 
-fig2 = px.bar(df2, x='age_group', y='avg_usage', color='age_group', title="2021년 하반기 연령대별 평균 응급 이용량")
+fig2 = px.bar(df2, x='age_group', y='avg_usage', color='age_group', title="2021년 하반기 연령대별 평균 응급실 이용량")
 st.plotly_chart(fig2, use_container_width=True)
 
 with st.expander("사용한 SQL 및 인사이트 보기"):
@@ -84,7 +84,7 @@ LIMIT 7
 df3 = run_query(sql3)
 
 fig3 = px.bar(df3, x='기관수', y='자치구', orientation='h', color='기관수', 
-             color_continuous_scale='Viridis', title="응급의료기관이 많은 상위 7개 지역")
+             color_continuous_scale='Viridis', title="서울시 내 응급의료기관이 많은 상위 7개 지역")
 st.plotly_chart(fig3, use_container_width=True)
 
 with st.expander("사용한 SQL 및 인사이트 보기"):
